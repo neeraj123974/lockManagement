cdc13b9eaca8cda10391af589463ee74e96ba0bd
neeraj <neerajsisodiya37@gmail.com>
2020-05-07 15:13:43 +0530

    apply the cors module in server.js

--- server/src/server.js
+++ server/src/server.js
@@ -4,23 +4,24 @@ import cookieParser from 'cookie-parser'
 import database from './config/database'
 import config from './config/config'
 import apis from './routes/router'
-
+const cors = require('cors')
 const app = express()
 
 // Allow cors and allow origin access
-app.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin',"*");
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  //Auth Each API Request created by user.
-  next();
-});
+// app.all('*', function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin',"*");
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//   );
+//   //Auth Each API Request created by user.
+//   next();
+// });
 
 // Use body-parser and cookie parser for use post data
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cookieParser())
